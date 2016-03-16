d5527ae45f68e2ef2457de83ea6e4ce515154cdd
Jazel Canseco <jcanseco@uwaterloo.ca>
2016-03-15 21:22:35 -0400

    Initial project setup

---
+++ app/src/main/java/com/jcanseco/radio/RadioPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.jcanseco.radio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadioPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_player);
+    }
+}
