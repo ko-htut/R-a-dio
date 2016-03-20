72c21d1bf4f27046ef34511c2d0ac22f0ef00ee2
Jazel Canseco <jcanseco@uwaterloo.ca>
2016-03-20 18:51:41 -0400

    Created RadioPlayerPresenter to contain the business logic for RadioPlayerActivity [#116095003]

---
+++ app/src/main/java/com/jcanseco/radio/radioplayer/RadioPlayerPresenter.java
@@ -0,0 +1,105 @@
+package com.jcanseco.radio.radioplayer;
+
+import com.jcanseco.radio.loaders.RadioContentLoader;
+import com.jcanseco.radio.models.Dj;
+import com.jcanseco.radio.models.NowPlayingTrack;
+import com.jcanseco.radio.models.RadioContent;
+
+public class RadioPlayerPresenter implements RadioContentLoader.RadioContentListener {
+
+    private RadioPlayerPresenter.View radioPlayerView;
+    private RadioContentLoader radioContentLoader;
+
+    private boolean isPlayerPlaying = false;
+    private String radioStreamUrl = null;
+
+    public RadioPlayerPresenter(RadioContentLoader radioContentLoader) {
+        this.radioContentLoader = radioContentLoader;
+        this.radioContentLoader.setRadioContentListener(this);
+    }
+
+    public void attachView(RadioPlayerPresenter.View radioPlayerView) {
+        this.radioPlayerView = radioPlayerView;
+    }
+
+    public void onResume() {
+        radioContentLoader.loadContent();
+    }
+
+    public void onActionButtonClicked() {
+        if (isPlayerPlaying()) {
+            pausePlayer();
+        } else {
+            playPlayer();
+        }
+    }
+
+    protected boolean isPlayerPlaying() {
+        return isPlayerPlaying;
+    }
+
+    protected void pausePlayer() {
+        radioPlayerView.showPlayButton();
+        isPlayerPlaying = false;
+    }
+
+    protected void playPlayer() {
+        String streamUrl = getStreamUrl();
+        if (streamUrl != null) {
+            radioPlayerView.showPauseButton();
+            radioPlayerView.loadRadioStream(streamUrl);
+            isPlayerPlaying = true;
+        } else {
+            radioPlayerView.showCouldNotLoadRadioStreamErrorMessage();
+        }
+    }
+
+    @Override
+    public void onRadioContentLoadSuccess(RadioContent radioContent) {
+        NowPlayingTrack currentTrack = radioContent.getCurrentTrack();
+        Dj currentDj = radioContent.getCurrentDj();
+
+        radioPlayerView.showCurrentTrackTitle(currentTrack.getTitle());
+        radioPlayerView.showCurrentDjName(currentDj.getName());
+        radioPlayerView.showCurrentDjAvatar(currentDj.getAvatarUrl());
+        radioPlayerView.showNumOfListeners(radioContent.getNumOfListeners());
+
+        setRadioStreamUrl(radioContent.getStreamUrl());
+    }
+
+    @Override
+    public void onRadioContentLoadFailed() {
+        radioPlayerView.showCouldNotLoadRadioContentErrorMessage();
+        pausePlayer();
+    }
+
+    protected String getStreamUrl() {
+        return radioStreamUrl;
+    }
+
+    protected void setRadioStreamUrl(String radioStreamUrl) {
+        this.radioStreamUrl = radioStreamUrl;
+    }
+
+
+    public interface View {
+
+        void showPlayButton();
+
+        void showPauseButton();
+
+        void showCurrentTrackTitle(String title);
+
+        void showCurrentDjName(String name);
+
+        void showCurrentDjAvatar(String avatarUrl);
+
+        void showNumOfListeners(int numOfListeners);
+
+        void loadRadioStream(String streamUrl);
+
+        void showCouldNotLoadRadioContentErrorMessage();
+
+        void showCouldNotLoadRadioStreamErrorMessage();
+    }
+}
---
+++ app/src/test/java/com/jcanseco/radio/radioplayer/RadioPlayerPresenterTest.java
@@ -0,0 +1,202 @@
+package com.jcanseco.radio.radioplayer;
+
+import com.jcanseco.radio.loaders.RadioContentLoader;
+import com.jcanseco.radio.models.fakes.FakeRadioContent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RadioPlayerPresenterTest {
+
+    private RadioPlayerPresenter radioPlayerPresenter;
+
+    @Mock
+    private RadioContentLoader radioContentLoader;
+
+    @Mock
+    private RadioPlayerPresenter.View radioPlayerView;
+
+    @Before
+    public void setup() {
+        radioPlayerPresenter = spy(new RadioPlayerPresenter(radioContentLoader));
+        radioPlayerPresenter.attachView(radioPlayerView);
+    }
+
+    @Test
+    public void onResume_shouldLoadRadioContent() {
+        radioPlayerPresenter.onResume();
+
+        verify(radioContentLoader).loadContent();
+    }
+
+    @Test
+    public void playerShouldBePausedByDefault() {
+        assertThat(radioPlayerPresenter.isPlayerPlaying()).isFalse();
+    }
+
+    @Test
+    public void onActionButtonClicked_ifPlayerIsPlaying_thenPausePlayer() {
+        when(radioPlayerPresenter.isPlayerPlaying()).thenReturn(true);
+
+        radioPlayerPresenter.onActionButtonClicked();
+
+        verify(radioPlayerPresenter).pausePlayer();
+    }
+
+    @Test
+    public void onActionButtonClicked_ifPlayerIsPaused_thenPlayPlayer() {
+        when(radioPlayerPresenter.isPlayerPlaying()).thenReturn(false);
+
+        radioPlayerPresenter.onActionButtonClicked();
+
+        verify(radioPlayerPresenter).playPlayer();
+    }
+
+    @Test
+    public void whenPlayerPaused_thenShowPlayButton() {
+        radioPlayerPresenter.pausePlayer();
+
+        verify(radioPlayerView).showPlayButton();
+    }
+
+    @Test
+    public void whenPlayerPaused_thenChangePlayerStateToPaused() {
+        radioPlayerPresenter.pausePlayer();
+
+        assertThat(radioPlayerPresenter.isPlayerPlaying()).isFalse();
+    }
+
+    @Test
+    public void whenPlayerPlayed_andStreamUrlNotNull_thenChangePlayerStateToPlaying() {
+        when(radioPlayerPresenter.getStreamUrl()).thenReturn("https://streamurl.com");
+
+        radioPlayerPresenter.playPlayer();
+
+        assertThat(radioPlayerPresenter.isPlayerPlaying()).isTrue();
+    }
+
+    @Test
+    public void whenPlayerPlayed_andRadioStreamUrlNotNull_thenDontChangePlayerStateToPlaying() {
+        when(radioPlayerPresenter.getStreamUrl()).thenReturn(null);
+
+        radioPlayerPresenter.playPlayer();
+
+        assertThat(radioPlayerPresenter.isPlayerPlaying()).isFalse();
+    }
+
+    @Test
+    public void whenPlayerPlayed_andRadioStreamUrlNotNull_thenShowPauseButton() {
+        when(radioPlayerPresenter.getStreamUrl()).thenReturn("https://streamurl.com");
+
+        radioPlayerPresenter.playPlayer();
+
+        verify(radioPlayerView).showPauseButton();
+    }
+
+    @Test
+    public void whenPlayerPlayed_andRadioStreamUrlIsNull_thenDontShowPauseButton() {
+        when(radioPlayerPresenter.getStreamUrl()).thenReturn(null);
+
+        radioPlayerPresenter.playPlayer();
+
+        verify(radioPlayerView, never()).showPauseButton();
+    }
+
+    @Test
+    public void whenPlayerPlayed_andRadioStreamUrlNotNull_thenLoadRadioStreamOntoView() {
+        when(radioPlayerPresenter.getStreamUrl()).thenReturn("https://streamurl.com");
+
+        radioPlayerPresenter.playPlayer();
+
+        verify(radioPlayerView).loadRadioStream("https://streamurl.com");
+    }
+
+    @Test
+    public void whenPlayerPlayed_andRadioStreamUrlIsNull_thenDontLoadRadioStreamOntoView() {
+        when(radioPlayerPresenter.getStreamUrl()).thenReturn(null);
+
+        radioPlayerPresenter.playPlayer();
+
+        verify(radioPlayerView, never()).loadRadioStream(anyString());
+    }
+
+    @Test
+    public void whenPlayerPlayed_andRadioStreamUrlIsNull_thenShowCouldNotLoadRadioStreamErrorMessage() {
+        when(radioPlayerPresenter.getStreamUrl()).thenReturn(null);
+
+        radioPlayerPresenter.playPlayer();
+
+        verify(radioPlayerView).showCouldNotLoadRadioStreamErrorMessage();
+    }
+
+    @Test
+    public void onRadioContentLoadSuccess_shouldShowCurrentTrackTitle() {
+        FakeRadioContent radioContent = new FakeRadioContent();
+
+        radioPlayerPresenter.onRadioContentLoadSuccess(radioContent);
+
+        verify(radioPlayerView).showCurrentTrackTitle("current track title");
+    }
+
+    @Test
+    public void onRadioContentLoadSuccess_shouldShowCurrentDjName() {
+        FakeRadioContent radioContent = new FakeRadioContent();
+
+        radioPlayerPresenter.onRadioContentLoadSuccess(radioContent);
+
+        verify(radioPlayerView).showCurrentDjName("current dj name");
+    }
+
+    @Test
+    public void onRadioContentLoadSuccess_shouldShowCurrentDjAvatar() {
+        FakeRadioContent radioContent = new FakeRadioContent();
+
+        radioPlayerPresenter.onRadioContentLoadSuccess(radioContent);
+
+        verify(radioPlayerView).showCurrentDjAvatar("http://r-a-d.io/api/dj-image/20");
+    }
+
+    @Test
+    public void onRadioContentLoadSuccess_shouldShowNumOfListeners() {
+        FakeRadioContent radioContent = new FakeRadioContent();
+
+        radioPlayerPresenter.onRadioContentLoadSuccess(radioContent);
+
+        verify(radioPlayerView).showNumOfListeners(253);
+    }
+
+    @Test
+    public void onRadioContentLoadSuccess_shouldSaveRadioStreamUrl() {
+        FakeRadioContent radioContent = new FakeRadioContent();
+
+        radioPlayerPresenter.onRadioContentLoadSuccess(radioContent);
+
+        verify(radioPlayerPresenter).setRadioStreamUrl("https://streamurl.com");
+        assertThat(radioPlayerPresenter.getStreamUrl()).isEqualTo("https://streamurl.com");
+    }
+
+    @Test
+    public void onRadioContentLoadFailed_shouldShowCouldNotLoadRadioContentErrorMessage() {
+        radioPlayerPresenter.onRadioContentLoadFailed();
+
+        verify(radioPlayerView).showCouldNotLoadRadioContentErrorMessage();
+    }
+
+    @Test
+    public void onRadioContentLoadFailed_shouldPausePlayer() {
+        radioPlayerPresenter.onRadioContentLoadFailed();
+
+        verify(radioPlayerPresenter).pausePlayer();
+    }
+}
