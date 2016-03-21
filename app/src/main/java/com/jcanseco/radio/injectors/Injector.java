1023a5e3214e3900904f177d3cbc96ea2ba6332a
Jazel Canseco <jcanseco@uwaterloo.ca>
2016-03-21 19:18:22 -0400

    Added Injector class responsible for injecting dependencies such as presenters into the views

---
+++ app/src/main/java/com/jcanseco/radio/injectors/Injector.java
@@ -0,0 +1,21 @@
+package com.jcanseco.radio.injectors;
+
+import com.jcanseco.radio.api.RadioRestService;
+import com.jcanseco.radio.api.RestClient;
+import com.jcanseco.radio.loaders.RadioContentLoader;
+import com.jcanseco.radio.radioplayer.RadioPlayerPresenter;
+
+public class Injector {
+
+    public static RadioRestService provideRadioRestService() {
+        return RestClient.getRadioRestService();
+    }
+
+    public static RadioContentLoader provideRadioContentLoader() {
+        return new RadioContentLoader(provideRadioRestService());
+    }
+
+    public static RadioPlayerPresenter provideRadioPlayerPresenter() {
+        return new RadioPlayerPresenter(provideRadioContentLoader());
+    }
+}
